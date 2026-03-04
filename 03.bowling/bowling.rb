8e14d3abcff75b2b00745c32b1b2a55bf046ae3f
shin-fkmt <fkmt.shin@gmail.com>
2026-03-04 16:14:08 +0900

    framesの値を個別に変数定義するよう修正（可読性向上のため）

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,14 +4,18 @@
 frames = ARGV[0].gsub(/X/, '10,0').split(',').map(&:to_i).each_slice(2).to_a
 
 point = (0..frames.size - 1).sum do |i|
+  current_frame = frames[i]
+  next_frame = frames[i + 1]
+  after_next_frame = frames[i + 2]
+
   if i > 8
-    frames[i].sum
-  elsif frames[i][0] == 10
-    frames[i].sum + frames[i + 1].sum + (frames[i + 1][0] == 10 ? frames[i + 2][0] : 0)
-  elsif frames[i].sum == 10
-    frames[i].sum + frames[i + 1][0]
+    current_frame.sum
+  elsif current_frame[0] == 10
+    current_frame.sum + next_frame.sum + (next_frame[0] == 10 ? after_next_frame[0] : 0)
+  elsif current_frame.sum == 10
+    current_frame.sum + next_frame[0]
   else
-    frames[i].sum
+    current_frame.sum
   end
 end
 
