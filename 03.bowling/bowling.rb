8d4b83dd2b6b2f3896bca40461331a761ccc3d1a
shin-fkmt <fkmt.shin@gmail.com>
2026-03-04 16:08:05 +0900

    eachメソッドで行っていた合算処理をsumメソッドを利用した合算処理に修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,17 +3,15 @@
 
 frames = ARGV[0].gsub(/X/, '10,0').split(',').map(&:to_i).each_slice(2).to_a
 
-point = 0
-(0..frames.size - 1).each do |i|
+point = (0..frames.size - 1).sum do |i|
   if i > 8
-    point += frames[i].sum
+    frames[i].sum
   elsif frames[i][0] == 10
-    point += frames[i].sum + frames[i + 1].sum
-    point += frames[i + 2][0] if frames[i + 1][0] == 10
+    frames[i].sum + frames[i + 1].sum + (frames[i + 1][0] == 10 ? frames[i + 2][0] : 0)
   elsif frames[i].sum == 10
-    point += frames[i].sum + frames[i + 1][0]
+    frames[i].sum + frames[i + 1][0]
   else
-    point += frames[i].sum
+    frames[i].sum
   end
 end
 
