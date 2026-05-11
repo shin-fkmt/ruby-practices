0611cdc51f4b3fe7892347a0c6c2c57901773455
shin-fkmt <fkmt.shin@gmail.com>
2026-05-11 17:22:00 +0900

    合計行の各合計値算出処理を見直し

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -12,9 +12,9 @@ def main
   exit if file_details.empty?
 
   if file_details.size > 1
-    total_line_count = file_details.flat_map { _1.values_at(:line_count) }.sum
-    total_word_count = file_details.flat_map { _1.values_at(:word_count) }.sum
-    total_byte_count = file_details.flat_map { _1.values_at(:byte_count) }.sum
+    total_line_count = file_details.map { _1[:line_count] }.sum
+    total_word_count = file_details.map { _1[:word_count] }.sum
+    total_byte_count = file_details.map { _1[:byte_count] }.sum
     file_details << create_file_detail(total_line_count, total_word_count, total_byte_count, '合計', false)
   end
 
